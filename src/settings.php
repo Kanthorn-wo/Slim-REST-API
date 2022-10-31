a46eaa28949a2be69520c7de1b400698d9da693d
Kanthorn1995 <kanthorn.wo@rmuti.ac.th>
2022-10-31 17:32:12 +0700

    slim api

--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,12 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+        // Config connect database
+        'db' => [
+            'host' => 'localhost',
+            'dbname' => 'stockrestdb',
+            'user' => 'root',
+            'pass' => '',
+        ],
     ],
 ];
